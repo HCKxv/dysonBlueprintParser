0abb2678fe1c6d37eeea48d4e76c85b38789009d
HCK <hck369@petalmail.com>
2026-06-29 22:56:21 +0800

    1.0

--- fetchTextFile.js
+++
@@ -1,8 +0,0 @@
-export async function fetchTextFile(url) {
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error(`HTTP ${response.status}`);
-    }
-    const text = await response.text();
-    return text;
-}
